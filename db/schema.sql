9bb517a0b76992b20c54f3a3cadc9cb789a80263
Saikiran Punnam AWS Devops Engineer <punnamsaikiran2000@gmail.com>
2026-04-05 19:45:13 +0530

    Create employee_db and employees table

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE employees (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100),
+  email VARCHAR(100) UNIQUE,
+  role VARCHAR(50),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
